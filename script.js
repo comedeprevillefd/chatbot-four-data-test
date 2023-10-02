f356da74dc8784e6e514537d670086fa044da387
Côme de Préville <121291556+comedeprevillefd@users.noreply.github.com>
2023-10-02 17:25:02 +0200

    Update 4 script.js : la foireuse ?

--- script.js
+++ script.js
@@ -5,25 +5,29 @@ function sendMessage() {
     // Affiche le message de l'utilisateur dans le chat
     chatBox.innerHTML += `<div>User: ${userInput}</div>`;
 
-    // Crée un objet FormData pour envoyer les données
-    const formData = new FormData();
-    formData.append('message', userInput);
+    // Prépare le corps de la requête POST
+    const requestBody = {
+        message: userInput
+    };
 
     // Envoie la requête POST à l'API
     fetch('https://n8n.fourdata.io/webhook/chatbot-four-data-ff3c-ee-dijon', {
         method: 'POST',
         headers: {
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Content-Type': 'multipart/form-data; boundary=--------------------------048096710773151122201267',
-            // Ajoutez d'autres entêtes si nécessaire
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
         },
-        body: formData,
+        body: JSON.stringify(requestBody),
     })
     .then(response => response.json())
     .then(data => {
-        // Affiche la réponse de l'API dans le chat
-        chatBox.innerHTML += `<div>Chatbot: ${data.message}</div>`;
+        // Traite la réponse de l'API
+        if (Array.isArray(data) && data.length > 0) {
+            const chatbotResponse = data[0].text;
+            chatBox.innerHTML += `<div>Chatbot: ${chatbotResponse}</div>`;
+        } else {
+            console.error('Réponse inattendue de l\'API:', data);
+        }
     })
     .catch(error => console.error('Erreur lors de l\'envoi de la requête:', error));
 

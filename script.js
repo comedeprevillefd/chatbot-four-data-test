0be01e8b7a513b66c16bfc5dfbf8beb9ac5ce0f2
Côme de Préville <121291556+comedeprevillefd@users.noreply.github.com>
2023-10-02 17:40:40 +0200

    Update 6 script.js j'en peux plus

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ function sendMessage() {
     // Affiche le message de l'utilisateur dans le chat
     chatBox.innerHTML += `<div>User: ${userInput}</div>`;
 
-    // Crée un objet FormData pour envoyer les données
-    const formData = new FormData();
-    formData.append('message', userInput);
+    // Prépare le corps de la requête POST
+    const requestBody = {
+        message: userInput
+    };
 
     // Envoie la requête POST à l'API
     fetch('https://n8n.fourdata.io/webhook/chatbot-four-data-ff3c-ee-dijon', {
@@ -15,10 +16,9 @@ function sendMessage() {
         headers: {
             'Accept': '*/*',
             'Accept-Encoding': 'gzip, deflate, br',
-            'Content-Type': 'multipart/form-data; boundary=--------------------------048096710773151122201267',
-            // Ajoutez d'autres entêtes si nécessaire
+            'Content-Type': 'application/json', // Utilisation de JSON au lieu de multipart/form-data
         },
-        body: formData,
+        body: JSON.stringify(requestBody),
     })
     .then(response => response.json())
     .then(data => {

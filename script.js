c759dc2d203b41ec73c57605dc40103e63581b68
Côme de Préville <121291556+comedeprevillefd@users.noreply.github.com>
2023-10-02 17:33:24 +0200

    Update 5 script.js la bonne

--- script.js
+++ script.js
@@ -5,28 +5,32 @@ function sendMessage() {
     // Affiche le message de l'utilisateur dans le chat
     chatBox.innerHTML += `<div>User: ${userInput}</div>`;
 
-    // Prépare le corps de la requête POST
-    const requestBody = {
-        message: userInput
-    };
+    // Crée un objet FormData pour envoyer les données
+    const formData = new FormData();
+    formData.append('message', userInput);
 
     // Envoie la requête POST à l'API
     fetch('https://n8n.fourdata.io/webhook/chatbot-four-data-ff3c-ee-dijon', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Content-Type': 'multipart/form-data; boundary=--------------------------048096710773151122201267',
+            // Ajoutez d'autres entêtes si nécessaire
         },
-        body: JSON.stringify(requestBody),
+        body: formData,
     })
     .then(response => response.json())
     .then(data => {
-        // Traite la réponse de l'API
+        // Assurez-vous que la réponse est un tableau non vide
         if (Array.isArray(data) && data.length > 0) {
-            const chatbotResponse = data[0].text;
-            chatBox.innerHTML += `<div>Chatbot: ${chatbotResponse}</div>`;
+            // Récupère le texte de la première réponse
+            const botText = data[0].text;
+
+            // Affiche la réponse de l'API dans le chat
+            chatBox.innerHTML += `<div>Chatbot: ${botText}</div>`;
         } else {
-            console.error('Réponse inattendue de l\'API:', data);
+            console.error('Réponse de l\'API invalide:', data);
         }
     })
     .catch(error => console.error('Erreur lors de l\'envoi de la requête:', error));

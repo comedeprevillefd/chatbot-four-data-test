6d946db60e9ba2d70405ae9f55ee3d3aa6376a89
Côme de Préville <121291556+comedeprevillefd@users.noreply.github.com>
2023-10-02 15:53:47 +0200

    Refonte 1 script.js

--- script.js
+++ script.js
@@ -1,51 +1,25 @@
-// Remplacez 'YOUR_API_KEY' par votre clé API GPT-3.5
-const apiKey = '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';
-const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-
 function sendMessage() {
-    const userInput = document.getElementById('user-input');
-    const userMessage = userInput.value.trim();
+    const userInput = document.getElementById('user-input').value;
+    const chatBox = document.getElementById('chat-box');
 
-    if (userMessage === '') {
-        return;
-    }
+    // Affiche le message de l'utilisateur dans le chat
+    chatBox.innerHTML += `<div>User: ${userInput}</div>`;
 
-    appendMessage('user', userMessage);
-
-    // Ajoutez le préfixe demandé à la prompt
-    const prompt = "Tu es un assistant spécialiste des combustibles. Réponds à ceci : " + userMessage;
-
-    // Envoie la requête à l'API GPT-3.5
-    fetch(apiUrl, {
+    // Envoie la requête POST à l'API
+    fetch('https://n8n.fourdata.io/webhook-test/chatbot-four-data-ff3c-ee-dijon', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
         },
-        body: JSON.stringify({
-            prompt: prompt,
-            max_tokens: 150
-        })
+        body: JSON.stringify({ message: userInput }),
     })
     .then(response => response.json())
     .then(data => {
-        const gptResponse = data.choices[0].text.trim();
-        appendMessage('assistant', gptResponse);
+        // Affiche la réponse de l'API dans le chat
+        chatBox.innerHTML += `<div>Chatbot: ${data.message}</div>`;
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => console.error('Erreur lors de l\'envoi de la requête:', error));
 
-    // Efface l'input utilisateur
-    userInput.value = '';
-}
-
-function appendMessage(sender, message) {
-    const chatContainer = document.getElementById('chat-container');
-    const chat = document.getElementById('chat');
-    const newMessage = document.createElement('div');
-    newMessage.className = `message ${sender}`;
-    newMessage.textContent = message;
-    chat.appendChild(newMessage);
-
-    // Fait défiler vers le bas pour afficher le dernier message
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+    // Efface le champ de saisie
+    document.getElementById('user-input').value = '';
 }

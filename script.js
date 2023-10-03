a7edfc61be9bff703d18651b13f2d53d31aac974
Côme de Préville <121291556+comedeprevillefd@users.noreply.github.com>
2023-10-03 11:45:14 +0200

    Update script.js final 2

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function sendMessage() {
-
     const userInput = document.getElementById('user-input').value;
     const chatBox = document.getElementById('chat-box');
-    
-    // Affiche le message de l'utilisateur dans le chat
-    chatBox.innerHTML += `<div><b>Moi :</b> ${userInput}</div>`;
+    const loader = document.getElementById('loader');
+
+    // Affiche le message de l'utilisateur dans le chat avec l'animation de chargement
+    chatBox.innerHTML += `
+        <div class="user-message">
+            <b>Moi :</b> ${userInput}
+            <div class="loader"></div>
+        </div>
+    `;
+
+    // Afficher l'animation de chargement
+    loader.style.display = 'inline-block';
 
     // Crée un objet FormData pour envoyer les données
     const formData = new FormData();
@@ -29,18 +37,29 @@ function sendMessage() {
     .then(response => response.json())
     .then(data => {
         console.log('Réponse de l\'API:', data);
-    
+
         // Assurez-vous que la réponse est un objet non vide
         if (typeof data === 'object' && data.text) {
             // Récupère le texte de la réponse
             const botText = data.text;
             console.log('Texte du chatbot:', botText);
-    
+
+            // Supprime l'animation de chargement du message de l'utilisateur
+            const userMessage = chatBox.lastElementChild;
+            userMessage.removeChild(userMessage.lastElementChild);
+
             // Affiche la réponse de l'API dans le chat
             const chatbotMessage = document.createElement('div');
             chatbotMessage.className = 'chatbot-message';
             chatbotMessage.innerHTML = `<div><b>FD Chatbot :</b> ${botText}</div>`;
             chatBox.appendChild(chatbotMessage);
+
+            // Défilement automatique vers le bas avec une animation en douceur
+            chatBox.scrollTo({
+                top: chatBox.scrollHeight,
+                behavior: 'smooth'
+            });
+
             console.log('Message du chatbot ajouté à la boîte de chat:', chatbotMessage);
         } else {
             console.error('Réponse de l\'API invalide:', data);
